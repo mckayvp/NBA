27cd377214b32fae21405210912f76994dd08275
mckayvp <mckaypalmer@gmail.com>
2017-11-30 22:26:30 -0700

    Created solution, added home page and conference pages

---
+++ WebApplication1/WebApplication1/Controllers/TeamsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TeamsController : Controller
+    {
+        // GET: Teams
+        public ActionResult Index(int teamID)
+        {
+            //Put teamID in Viewbag to display in the View
+            ViewBag.teamNumber = teamID;
+            return View();
+        }
+    }
+}
